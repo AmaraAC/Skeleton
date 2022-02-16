faa61c46512a70d0afe01f108807f97d83d27d71
Amarachukwu Abananne <P2590422@my365.dmu.ac.uk>
2022-02-16 11:51:40 +0000

    Created properties for my table

--- Testing4/tstOrder.cs
+++ Testing4/tstOrder.cs
@@ -14,5 +14,53 @@ namespace Testing4
             Assert.IsNotNull(anOrder);
             
         }
+        [TestMethod]
+        public void DispatchedpropertOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Boolean TestData = true;
+            AnOrder.Dispatched = TestData;
+            Assert.AreEqual(AnOrder.Dispatched, TestData);
+        }
+        [TestMethod]
+        public void DateAddedPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            DateTime TestData = DateTime.Now.Date;
+            AnOrder.DateAdded = TestData;
+            Assert.AreEqual(AnOrder.DateAdded, TestData);
+        }
+        [TestMethod]
+        public void OrderIDPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            int TestData = 3;
+            AnOrder.OrderID = TestData;
+            Assert.AreEqual(AnOrder.OrderID, TestData);
+        }
+        [TestMethod]
+        public void OrderAddressPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            String TestData = "17 Trap Road";
+            AnOrder.OrderAddress = TestData;
+            Assert.AreEqual(AnOrder.OrderAddress, TestData);
+        }
+        [TestMethod]
+        public void CustumerNamePropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            String TestData = "Fetty Wap";
+            AnOrder.CustumerName = TestData;
+            Assert.AreEqual(AnOrder.CustumerName, TestData);
+        }
+        [TestMethod]
+        public void FinalPricePropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Double TestData = 20.25;
+            AnOrder.FinalPrice = TestData;
+            Assert.AreEqual(AnOrder.FinalPrice, TestData);
+        }
     }
 }
