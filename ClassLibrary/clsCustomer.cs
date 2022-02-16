a8b0aaca6aa57eff548db141548a39ba5d74c1e2
Ismail Fagbenro <P2612171@my365.dmu.ac.uk>
2022-02-16 18:29:24 +0000

    Properties to the table

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,6 +1,18 @@
-﻿namespace ClassLibrary
+﻿
+using System;
+namespace ClassLibrary
 {
     public class clsCustomer
     {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+
+        public DateTime DateJoined { get; set; }
+
+        public bool Over18 { get; set; }
+
+        public string Gender { get; set; }
+
+        public bool MemberSubscription { get; set; }
     }
 }
