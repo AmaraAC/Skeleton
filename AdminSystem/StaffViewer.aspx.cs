e97565733f7b08a3edb307285f21756084bd8d89
Ibukunoluwa Ibrahim <P2604156@my365.dmu.ac.uk>
2022-02-21 17:32:53 +0000

    ibz staff all done

--- AdminSystem/StaffViewer.aspx.cs
+++ AdminSystem/StaffViewer.aspx.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        clsStaff AStaff = new clsStaff();
+        AStaff = (clsStaff)Session["AnStaff"];
+        Response.Write(AStaff.StaffName);
+        Response.Write(AStaff.Gender);
+        Response.Write(AStaff.JoinDate);
+        Response.Write(AStaff.StaffId);
+        Response.Write(AStaff.StaffName);
+        Response.Write(AStaff.Role);
+
+
 
     }
 }
