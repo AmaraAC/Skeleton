a893d887e0b9890d2efded3e2a4f14a1bf2ad753
Ismail Fagbenro <P2612171@my365.dmu.ac.uk>
2022-03-14 14:51:59 +0000

    almost done i think

--- Testing2/tstCust.cs
+++ Testing2/tstCust.cs
@@ -7,6 +7,12 @@ namespace Testing2
     [TestClass]
     public class tstCust
     {
+        //good test data
+        //create some test data to pass the method
+
+        string CustomerName = "Jake";
+        string DateJoined = DateTime.Now.Date.ToString();
+        string Gender = "Male";
         [TestMethod]
         public void InstanceOk()
         {
@@ -215,5 +221,337 @@ namespace Testing2
 
             Assert.IsTrue(OK);
         }
+        [TestMethod]
+        public void ValidMethodOK()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreEqual(Error, "");
+        }
+        [TestMethod]
+        public void CustomerNameMinLessOne()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+            string CustomerName = "";
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreNotEqual(Error, "");
+        }
+        [TestMethod]
+        public void CustomerNameMin()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+            string CustomerName = "g";
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreEqual(Error, "");
+        }
+        [TestMethod]
+            public void CustomerNameMinPlusOne()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+            string CustomerName = "gg";
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreEqual(Error, "");
+        }
+        [TestMethod]
+            public void CustomerNameMaxLessOne()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+            string CustomerName = "ggggg";
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreEqual(Error, "");
+        }
+        [TestMethod]
+            public void CustomerNameMax()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+            string CustomerName = "gggggg";
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreEqual(Error, "");
+        }
+        [TestMethod]
+            public void CustomerNameMaxPlusOne()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+            string CustomerName = "ggggggg";
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreNotEqual(Error, "");
+        }
+        [TestMethod]
+            public void CustomerNameMid()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+            string CustomerName = "ggg";
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreEqual(Error, "");
+        }
+        [TestMethod]
+            public void CustomerNameExtremeMax()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+            string CustomerName = "";
+            CustomerName = CustomerName.PadRight(500, 'g');
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreNotEqual(Error, "");
+        }
+        [TestMethod]
+        public void DateJoinedExtremeMin()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+            DateTime TestDate;
+
+            TestDate = DateTime.Now.Date;
+
+            TestDate = TestDate.AddYears(-100);
+
+            string DateJoined = TestDate.ToString();
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreNotEqual(Error, "");
+
+        }
+        [TestMethod]
+             public void DateJoinedMinLessOne()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+            DateTime TestDate;
+
+            TestDate = DateTime.Now.Date;
+
+            TestDate = TestDate.AddDays(-1);
+
+            string DateJoined = TestDate.ToString();
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreNotEqual(Error, "");
+        }
+        [TestMethod]
+             public void DateJoinedMin()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+            DateTime TestDate;
+
+            TestDate = DateTime.Now.Date;
+
+
+            string DateJoined = TestDate.ToString();
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreNotEqual(Error, "");
+        }
+        [TestMethod]
+             public void DateJoinedMinPlusOne()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+            DateTime TestDate;
+
+            TestDate = DateTime.Now.Date;
+
+            TestDate = TestDate.AddDays(1);
+
+            string DateJoined = TestDate.ToString();
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreNotEqual(Error, "");
+        }
+        [TestMethod]
+             public void DateJoinedExtremeMax()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+            DateTime TestDate;
+
+            TestDate = DateTime.Now.Date;
+
+            TestDate = TestDate.AddYears(100);
+
+            string DateJoined = TestDate.ToString();
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreNotEqual(Error, "");
+        }
+        [TestMethod]
+             public void DateJoinedInvalidData()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+
+
+            string DateJoined = "this is not a date";
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreNotEqual(Error, "");
+        }
+        [TestMethod]
+        public void GenderMinLessOne()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+            string Gender = "";
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreNotEqual(Error, "");
+        }
+        [TestMethod]
+        public void GenderMin()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+            string  Gender = "g";
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreEqual(Error, "");
+        }
+        [TestMethod]
+            public void GenderMinPlusOne()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+            string Gender = "gg";
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreEqual(Error, "");
+        }
+        [TestMethod]
+            public void GenderMaxLessOne()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+            string Gender = "ggggg";
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreEqual(Error, "");
+        }
+        [TestMethod]
+            public void GenderMax()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+            string Gender = "gggggg";
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreEqual(Error, "");
+        }
+        [TestMethod]
+            public void GenderMaxPlusOne()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+            string Gender = "ggggggg";
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreNotEqual(Error, "");
+        }
+        [TestMethod]
+            public void GenderMid()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+            string Gender = "ggg";
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreEqual(Error, "");
+        }
+        [TestMethod]
+            public void GenderExtremeMax()
+        {
+            clsCustomer gg = new clsCustomer();
+
+            String Error = "";
+
+            string Gender = "";
+            Gender = Gender.PadRight(500, 'g');
+
+            Error = gg.Valid(CustomerName, DateJoined, Gender);
+
+            Assert.AreNotEqual(Error, "");
+        }
     }
+
 }
